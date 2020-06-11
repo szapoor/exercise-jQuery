268b5e70ac3d521964fb3564a4f741a0ff976b18
szapoor <szapoor@users.noreply.github.com>
2020-06-11 15:30:58 +0200

    Exercise with jQuery part1-2

---
+++ exercise-jquery-part2/scripts.js
@@ -0,0 +1,7 @@
+let color = "purple";
+let number = "10";
+let word = "cool";
+
+$('.first-one').css('background-color','purple');
+$('.second').text('just a regular number,please');
+$('.third').text('Power of DOM');
---
+++ scripts.js
@@ -0,0 +1,5 @@
+$('#first').text('Macrotis');
+$('#second').css('border-color', 'purple');
+$('#first').css('background-color', 'yellow');
+$('#second').css('background-color', 'yellow');
+$('#third').css('background-color', 'yellow');
